6920e49d6b7d67cd0783ffcf86108f558fc98ec3
Taku Kitamura <tikutaku0128@icloud.com>
2018-02-04 11:19:10 +0900

    ファイルサーバーに存在刷るファイルは、利用ユ ーザーはアップロードできないよう変更

--- upload.sh
+++ upload.sh
@@ -27,8 +27,10 @@ host="13.231.55.13"
 
 if [ $# -eq 1 ]; then
   option=$1
-  if [ $option = "-l" ]; then
+  if [ $option = "-t" ]; then
     ssh -i $identifyFilePath $relayPointUser@$host "cat ~/.tree"
+  elif [ $option = "-l" ]; then
+    ssh -i $identifyFilePath $relayPointUser@$host "cat ~/.absolutePathListOfFileServer"
   fi
 
 elif [ $# -eq 2 ]; then
@@ -38,7 +40,15 @@ elif [ $# -eq 2 ]; then
   # この場合 $absoluteFileServerPath は abc/def/
 
   localFileOrDirectoryPath=$1
+
+  if [ ! -e $localFileOrDirectoryPath ]; then
+    echo "ローカル上の、 $localFileOrDirectoryPath が存在しません。"
+    echo "終了します。"
+    exit 2
+  fi
+
   absoluteFileServerPath=$2
+
 
   # $[before|after]OperationabsoluteFileServerPathTemp には、 /tmp/tmp.dpgp9A7UxD などが格納される
   # 下記の処理で、一時ファイルを利用しているかは、(もっとスマートな実装ができる気がする)
@@ -54,13 +64,39 @@ elif [ $# -eq 2 ]; then
 
   # 生成したディレクトリ内に第一引数（localFileOrDirectoryPath）を入れる
 
+  # ./upload.sh /Users/kitamurataku/downloads/exam2015.pdf /home/ri-one/share/abc
+
+  # 絶対パスからベースネームを取得
+  # ex. $ basename /Users/kitamurataku/downloads/test.txt
+  #       test.txt
+  baseFileOrDirectoryName=`basename $localFileOrDirectoryPath`
+  echo $baseFileOrDirectoryName
+
+  # ファイルサーバーにアップロード予定の、絶対パス
+  willUploadFileAbsolutePath=`echo "$absoluteFileServerPath/$baseFileOrDirectoryName" | sed -e 's/\/\//\//g'`
+  echo $willUploadFileAbsolutePath
+  echo "abc"
+
+  uploadFileAbsolutePath=`ssh -i $identifyFilePath $relayPointUser@$host "cat ~/.absolutePathListOfFileServer | grep -x $willUploadFileAbsolutePath"`
+  echo $uploadFileAbsolutePath
+
+  # ファイルサーバーにアップロードする予定のファイルサーバーのパスに、ファイルまたは、ディレトリが存在する時
+  # つまり、ファイルをアップロードできない時
+  if [ -n "$uploadFileAbsolutePath" ]; then
+    echo "ファイルサーバー上に、 $willUploadFileAbsolutePath が存在します。"
+    echo "終了します。"
+    exit 2
+  fi
+
   # 経由するインスタンス上に、ローカルファイルをアップロード
   # ファイル、シンボリックリンクの場合
   if [ -f $localFileOrDirectoryPath ]; then
+    echo "ファイルをアップロードします。"
     scp -i $identifyFilePath $localFileOrDirectoryPath $relayPointUser@$host:$relayServerTempDirectoryAbsolutePath
 
     # ディレクトリの場合
   else
+    echo "ディレクトリをアップロードします。"
     scp -i $identifyFilePath -r $localFileOrDirectoryPath $relayPointUser@$host:$relayServerTempDirectoryAbsolutePath
   fi
 
@@ -71,7 +107,7 @@ elif [ $# -eq 2 ]; then
 
 else
   echo "第一引数はファイルまたはディレクトリの絶対パス"
-  echo "第二引数はファイルサーバーの/home/share/ 以下のディレクトリの絶対パスが必要です。"
+  echo "第二引数ディレクトリの絶対パスが必要です。"
   echo "詳しくは、README を確認してください。"
   exit 2
 fi
